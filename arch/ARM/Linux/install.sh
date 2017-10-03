65357765d24f2da73c74be814978ef30b6860235
chainsx <chainsx@users.noreply.github.com>
2017-10-03 03:05:16 -0500

    Update install.sh

--- arch/ARM/Linux/install.sh
+++ arch/ARM/Linux/install.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+apt-get install nginx
+cd /etc/nginx/site-available
+rm default
+wget https://raw.githubusercontent.com/Erblocker/Pandora-magicbox/master/arch/ARM/Linux/data/default
+cd /var/www/html
+mkdir wmexam
+mkdir wmstudyservice.WSDL
+wget https://raw.githubusercontent.com/Erblocker/Pandora-magicbox/master/arch/ARM/Linux/data/wmexam/wmstudyservice.WSDL/index.xml
+service nginx restart
