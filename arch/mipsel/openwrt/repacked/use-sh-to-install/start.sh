57ffeb017d1474ea154db93d018a822457d62f4b
chainsx <chainsx@users.noreply.github.com>
2017-10-03 03:14:34 -0500

    Update start.sh

--- arch/mipsel/openwrt/repacked/use-sh-to-install/start.sh
+++ arch/mipsel/openwrt/repacked/use-sh-to-install/start.sh
@@ -1,6 +1,11 @@
 #!/bin/ash
-cd /tmp
-wget https://raw.githubusercontent.com/Erblocker/Pandora-magicbox/master/arch/mipsel/openwrt/repacked/use-sh-to-install/nginx.zip
-unzip /tmp/nginx.zip
-cd nginx
-sh install.sh
+opkg install nginx
+cd /etc/nginx
+rm nginx.conf
+wget https://raw.githubusercontent.com/Erblocker/Pandora-magicbox/master/arch/mipsel/openwrt/unpacked/data/etc/nginx/nginx.conf
+cd /var/www/html
+mkdir wmexam
+cd wmexam
+mkdir wmstudyservice.WSDL
+cd *.WSDL
+wget https://raw.githubusercontent.com/Erblocker/Pandora-magicbox/master/arch/mipsel/openwrt/unpacked/data/var/www/html/wmexam/wmstudyservice.WSDL/index.xml
